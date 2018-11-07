d0ef651bf7a41a42c8edb6b1e73092f0721a78a6
prquan <quanpr@foxmail.com>
2018-11-07 12:09:30 -0800

    Update hw.py

--- hw.py
+++ hw.py
@@ -5,7 +5,14 @@ matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 import math
 
-default_map = []
+default_map = [[255]*512 for i in range(512)]
+for i in range(100, 200):
+	default_map[i][200:500] = [0]*(500-200)
+for i in range(400,500):
+	default_map[i][100:300] = [0]*(300-100)
+for j in range(200, 400):
+	default_map[j][200:300] = [125]*(300-200)
+
 
 class robot:
 	def __init__(self, initial_idx, goal_idx, Da=8, e_space=default_map):
@@ -15,7 +22,7 @@ class robot:
 		self.o_space = o_space
 		self.max_fail = 10**4
 		self.found_path = False
-		# each node: 'idx':(x, y, theta), 'parent' = #, 'distance' = d
+		# each node: 'idx':(x, y, theta), 'parent' = parent number, 'distance' = d
 		self.RRTree = []
 		self.Dx = len(o_space)
 		self.Dy = len(o_space[0])
@@ -43,7 +50,36 @@ class robot:
 		path = self.generate_new_path(start, end)
 		return all([check_node(node) for node in path])
 
-	def nearest_neighbor(self, num_of_neighbor, target_idx):
+	def nearest_k_neighbor(self, target_idx, num_of_neighbor=1):
+		def distance(start, end):
+			#return (1/(sum(len(start)))*sum([(start[i]-end[i])**2 for i in range(len(start))]))**0.5
+			return (1/2*((start[0]-end[0])**2+(start[1]-end[1])))**0.5
 		if num_of_neighbor == 1:
-			
+			# j: distance, i: correpsonding index
+			d = [(j, i) for i, j in enumerate([distance(target_idx, node_idx['idx']) for node_idx in self.RRTree])]
+			nearest =  min(d, key=lambda x:x[0])[1]
+			return self.RRTree[nearest]['idx']
+		else:
+			import heapq
+			# j: distance, i: correpsonding index
+			d = [(j, i) for i, j in enumerate([distance(target_idx, node_idx['idx']) for node_idx in self.RRTree])]
+			result = []
+			for idx in d:
+				if len(result) < num_of_neighbor:
+					heapq.heappush(result, idx)
+				else:
+					heap.heappushpop(result, idx)
+			return [r[1] for r in result]
+
+	def generate_new_node(self, start, rand):
+		dx, dy = rand[0]-start[0], rand[1]-start[1]
+		if dx == 0:
+			return (x, y + self.Da, rand[2])
+		theta = np.arctan(abs(dy/dx))
+		x = start[0] + self.Da*np.cos(theta) if dx > 0 else start[0] - self.Da*np.cos(theta)
+		y = start[1] + self.Da*np.sin(theta) if dy > 0 else start[1] - self.Da*np.sin(theta)
+		return (x, y, rand[2])
+
+
+
 
