72c769fca7e5c025cbe8a581f63305fc3153af9e
prquan <quanpr@foxmail.com>
2018-11-07 13:20:06 -0800

    Update hw.py

--- hw.py
+++ hw.py
@@ -4,6 +4,7 @@ import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 import math
+import random
 
 default_map = [[255]*512 for i in range(512)]
 for i in range(100, 200):
@@ -27,28 +28,45 @@ class robot:
 		self.Dx = len(o_space)
 		self.Dy = len(o_space[0])
 		self.theta_range = 18
+		self.bias = 0.8
+		self.threshold = 10
+
+	def determine_angle(self, start, end):
+		dx, dy = rand[0]-start[0], rand[1]-start[1]
+		if dx == 0:
+			return np.pi/2 if dy > 0 else 3/2*np.pi
+		theta = np.arctan(abs(dy/dx))
+		if dx > 0 and dy >= 0:
+			return theta
+		elif dx < 0 and dy >= 0:
+			return theta + np.pi/2
+		elif dx < 0 and dy < 0:
+			return theta + np.pi
+		else:
+			return theta + 3*2*np.pi
 
 	def generate_new_path(self, start, end):
 		result = []
 		dx, dy = end[0]-start[0], end[1]-start[1]
 		if dx == 0:
-			result = [(start[0], start[1]+i, start[2]) for i in range(1,dy+1)]
+			result = [(start[0], start[1]+i, end[2]) for i in range(1,dy+1)]
 		else:
 			for i in range(1, abs(dx)+1):
-				node = (start[0]+i, start[1]+int(i*dy/dx), start[2])
+				node = (start[0]+i, start[1]+int(i*dy/dx), end[2])
 				result.append(node)
 		return result
 
+	def check_node(self, node):
+		if self.o_space[node[0]][node[1]] == 0:
+			return True
+		elif self.o_space[node[0]][node[1]] == 125 and np.pi<=node[2]<=2*np.pi:
+			return True
+		else:
+			return False
+
 	def valiadate_new_node(self, start, end):
-		def check_node(node):
-			if self.o_space[node[0]][node[1]] == 0:
-				return True
-			elif self.o_space[node[0]][node[1]] == 125 and np.pi<=node[2]<=2*np.pi:
-				return True
-			else:
-				retrun False
 		path = self.generate_new_path(start, end)
-		return all([check_node(node) for node in path])
+		return all([self.check_node(node) for node in path])
 
 	def nearest_k_neighbor(self, target_idx, num_of_neighbor=1):
 		def distance(start, end):
@@ -57,8 +75,8 @@ class robot:
 		if num_of_neighbor == 1:
 			# j: distance, i: correpsonding index
 			d = [(j, i) for i, j in enumerate([distance(target_idx, node_idx['idx']) for node_idx in self.RRTree])]
-			nearest =  min(d, key=lambda x:x[0])[1]
-			return self.RRTree[nearest]['idx']
+			nearest =  min(d, key=lambda x:x[0])
+			return (self.RRTree[nearest[1]]['idx'], nearest[0])
 		else:
 			import heapq
 			# j: distance, i: correpsonding index
@@ -69,16 +87,59 @@ class robot:
 					heapq.heappush(result, idx)
 				else:
 					heap.heappushpop(result, idx)
-			return [r[1] for r in result]
+			return result
 
-	def generate_new_node(self, start, rand):
+	def generate_next_move(self, start, rand):
 		dx, dy = rand[0]-start[0], rand[1]-start[1]
 		if dx == 0:
 			return (x, y + self.Da, rand[2])
-		theta = np.arctan(abs(dy/dx))
-		x = start[0] + self.Da*np.cos(theta) if dx > 0 else start[0] - self.Da*np.cos(theta)
-		y = start[1] + self.Da*np.sin(theta) if dy > 0 else start[1] - self.Da*np.sin(theta)
-		return (x, y, rand[2])
+		else:
+			theta = np.arctan(abs(dy/dx))
+			x = start[0] + self.Da*np.cos(theta) if dx > 0 else start[0] - self.Da*np.cos(theta)
+			y = start[1] + self.Da*np.sin(theta) if dy > 0 else start[1] - self.Da*np.sin(theta)
+			return (max(0, min(x, self.Dx)), max(0, min(y, self.Dy)), rand[2])
+
+	def plot_tree(self):
+		plt.figure()
+		for node in self.RRTree:
+			if node['parent'] is not None:
+				plt.plot([node['idx'][0], self.RRTree[node['parent']]['idx'][0]],
+						[node['idx'][1], self.RRTree[node['parent']]['idx'][1]], '-b')
+		plt.plot(self.initial_idx[0], self.initial_idx[1], 'xr')
+		plt.plot(self.goal_idx[0], self.goal_idx[1], 'xr')
+		plt.grid()
+		plt.show()
+
+	def planning(self):
+
+		def distance(start, end):
+			return (1/2*((start[0]-end[0])**2+(start[1]-end[1])))**0.5
+
+		fail_attempt = 0
+		while fail_attempt <= self.max_fail:
+			if random.uniform(0,1) < self.bias:
+				random.seed(0)
+				rand_x, rand_y = random.randint(0, self.Dx-1), random.randint(0, self.Dy-1)
+			else:
+				rand_x, rand_y = self.goal_idx[0], self.goal_idx[1]
+			nearest = self.nearest_k_neighbor((rand_x, rand_y, 0), 1)
+			rand = (rand_x, rand_y, self.determine_angle(nearest[0], (rand_x, rand_y, 0)))
+
+			new_move = self.generate_next_move(nearest[0], rand)
+			if not self.valiadate_new_node(nearest[0], new_move):
+				fail_attempt += 1
+				continue
+
+			new_move_distance = self.RRTree[nearest[1]][distance] + distance(new_move, nearest[0])
+			self.RRTree.append({'idx': new_move,
+								'parent':nearest[1],
+								'distance':new_move_distance})
+
+			distance_to_goal = distance(nearest[0], self.goal_idx)
+			if distance_to_goal < self.threshold and \
+				valiadate_new_node(new_move, self.goal_idx):
+				self.found_path = True
+
 
 
 
